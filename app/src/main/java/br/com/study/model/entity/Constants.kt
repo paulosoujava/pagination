940d5aa5c39cc4b7711495c4368d0796768d8e5c
PAULO OLIVEIRA <paulosoujava@gmail.com>
2019-03-19 18:59:10 -0300

    Update Constants.kt

--- app/src/main/java/br/com/study/model/entity/Constants.kt
+++ app/src/main/java/br/com/study/model/entity/Constants.kt
@@ -1,4 +1,4 @@
 package br.com.study.model.entity
 
-const val API_KEY = "531f34e2ab0eaff3448031bbf5f44ff7"
-const val PRIVATE_KEY = "6d792d4cae43a1bda53d316b010565344fca4d64"
+const val API_KEY = "YOUR_KEY"
+const val PRIVATE_KEY = "YOUR_PRIVATE_KEY"
